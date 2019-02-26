58c769f3b3246df2c24fc089fb92b32eb8f52394
Kamen Boev <kamen.boev@gmail.com>
2019-02-26 13:08:59 +0000

    added dom parser and jQuery to handle more easily html elements

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
+/* global fetch */
 require('./polyfills');
 
-const productsUrlBase = 'https://www.ikea.bg/living-room/Living-room-storage/';
-const categoies = ['Bookcases', 'Shelving-units', 'living-room-modular-storage-systems'];
 
+// Get the products and add them to a queue
+const productsUrlBase = 'https://www.ikea.bg/living-room/Living-room-storage/';
+const categories = ['Bookcases', 'Shelving-units', 'living-room-modular-storage-systems/eket', 'living-room-modular-storage-systems/BESTA-system'];
+
+const getProductData = (url) => {
+    return fetch(url)
+        .then((response) => {
+            if(!response.ok) {
+                throw new Error('Invalid url');
+            }
+            
+            return response.text();
+        })
+        .then((html) => {
+            return require('./dom-parser/index')(html);
+        })
+        .then(($) => {
+            const title = $('title').html();
+            console.log(title);
+        })
+        .catch();
+};
+
+getProductData(productsUrlBase + categories[0]);
